fa3dd64dee204653c86633b5d6af7f8c68a4c0e7
Shaquan Robinson <shaquanjrobinson@gmail.com>
2021-04-10 11:57:21 -0500

    Added flask form

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,10 @@
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators, TextAreaField
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+
+description = TextAreaField(u'Description', [validators.DataRequired()])
+photo = FileField('Photo', validators=[
+    FileRequired(),
+    FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')
+])
